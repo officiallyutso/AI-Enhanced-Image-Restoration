99765a88c3a9efadefe9a5b44a910b23a24f44d9
officiallyutso <utsosarkar1@gmail.com>
2025-06-19 00:55:49 +0530

    feat: doc

--- setup.py
+++ setup.py
@@ -1,1 +1,7 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
