a3000f72b862c6edefae11b7c215e05debc30e0b
officiallyutso <utsosarkar1@gmail.com>
2025-06-19 00:42:53 +0530

    feat: model urls

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+
+# Model URLs
+MODEL_URLS = {
+    'realesr-general-x4v3.pth': "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth",
+    'GFPGANv1.4.pth': "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth",
+}
