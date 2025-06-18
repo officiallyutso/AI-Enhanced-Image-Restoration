3a13c7f3f216ec6ed7ee5e33fe8c0642cb51c152
officiallyutso <utsosarkar1@gmail.com>
2025-06-19 00:45:29 +0530

    update: enhancer

--- src/enhancer.py
+++ src/enhancer.py
@@ -4,3 +4,14 @@ from gfpgan.utils import GFPGANer
 from realesrgan.utils import RealESRGANer
 from basicsr.archs.srvgg_arch import SRVGGNetCompact
 from .config import *
+
+
+class ImageEnhancer:
+    """Main image enhancement class."""
+    
+    def __init__(self):
+        self.sr_model = None
+        self.face_enhancer = None
+        self._load_models()
+        
+    
