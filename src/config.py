223f4cd46538088068eb6bfc9f7290def686a012
officiallyutso <utsosarkar1@gmail.com>
2025-06-19 00:43:26 +0530

    feat: add model parameters

--- src/config.py
+++ src/config.py
@@ -11,3 +11,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODELS_DIR = os.path.join(BASE_DIR, 'models')
 REALESRGAN_MODEL_PATH = os.path.join(MODELS_DIR, 'realesr-general-x4v3.pth')
 GFPGAN_MODEL_PATH = os.path.join(MODELS_DIR, 'GFPGANv1.4.pth')
+
+# Model parameters
+REALESRGAN_SCALE = 4
+REALESRGAN_TILE = 0
+REALESRGAN_TILE_PAD = 10
+REALESRGAN_PRE_PAD = 0
+
+GFPGAN_UPSCALE = 10
+GFPGAN_ARCH = 'clean'
+GFPGAN_CHANNEL_MULTIPLIER = 2
