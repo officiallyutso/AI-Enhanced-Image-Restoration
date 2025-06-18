89093c34d3d977f4bf0796763463531a80f0e484
officiallyutso <utsosarkar1@gmail.com>
2025-06-19 00:43:08 +0530

    feat: model paths

--- src/config.py
+++ src/config.py
@@ -5,3 +5,9 @@ MODEL_URLS = {
     'realesr-general-x4v3.pth': "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth",
     'GFPGANv1.4.pth': "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth",
 }
+
+# Paths
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODELS_DIR = os.path.join(BASE_DIR, 'models')
+REALESRGAN_MODEL_PATH = os.path.join(MODELS_DIR, 'realesr-general-x4v3.pth')
+GFPGAN_MODEL_PATH = os.path.join(MODELS_DIR, 'GFPGANv1.4.pth')
